5a7f0d4b101ef77c53d3b186edc6ce34af6d758e
Will Dietz <w@wdtz.org>
2018-06-19 15:02:10 -0500

    asan

--- default.nix
+++ default.nix
@@ -2,13 +2,27 @@
 
 with import nixpkgs {};
 
+let
+
+  extraCFLAGS = [
+    "-g"
+    "-O1"
+    "-fsanitize=address"
+  ];
+
+  curl_asan = curl.overrideDerivation (o: {
+    NIX_CFLAGS_COMPILE = (o.NIX_CFLAGS_COMPILE or []) ++ extraCFLAGS;
+  });
+in
 stdenv.mkDerivation {
   name = "curl-test";
 
   nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ curl ];
+  buildInputs = [ curl_asan ];
 
   src = builtins.fetchGit ./.;
+
+  NIX_CFLAGS_COMPILE = extraCFLAGS;
 
   installPhase = ''
     install -D -t $out/bin test
