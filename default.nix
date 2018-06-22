3e48c030fbc466222bed8a600f25d10dfe35d6b9
Will Dietz <w@wdtz.org>
2018-06-22 13:14:07 -0500

    default.nix: build tests against a few curl variants
    * latest curl release (7.60)
    * 7.60 + fix1.patch
    * latest curl git

--- default.nix
+++ default.nix
@@ -12,7 +12,7 @@ let
     "-fsanitize=address"
   ];
 
-  curl_asan = curl.overrideAttrs(o: {
+  curl_asan = attrs: curl.overrideAttrs(o: {
     NIX_CFLAGS_COMPILE = (o.NIX_CFLAGS_COMPILE or []) ++ extraCFLAGS;
     separateDebugInfo = false;
     dontStrip = true;
@@ -21,26 +21,43 @@ let
       "--enable-debug"
       "--enable-curldebug"
     ];
-
-    patches = (o.patches or []) ++ [
-      ./fix1.patch
-    #  ./fix2.patch
-    ];
   });
-in
-stdenv.mkDerivation {
-  name = "curl-test";
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ curl_asan ];
+  buildTests = { name, curl ? curl }:
+    stdenv.mkDerivation {
+      inherit name;
 
-  src = builtins.fetchGit ./.;
+      nativeBuildInputs = [ pkgconfig ];
+      buildInputs = [ curl ];
 
-  NIX_CFLAGS_COMPILE = extraCFLAGS;
+      src = builtins.fetchGit ./.;
 
-  dontStrip = true;
+      NIX_CFLAGS_COMPILE = extraCFLAGS;
 
-  installPhase = ''
-    install -D -t $out/bin test multi-stress
-  '';
+      dontStrip = true;
+
+      installPhase = ''
+        install -D -t $out/bin test multi-stress
+      '';
+    };
+
+in {
+  use_curl_release = buildTests { name = "curl-release-tests"; curl = curl; };
+  use_curl_fix1 = buildTests {
+    name = "curl-fix1-tests";
+    curl = curl.overrideAttrs (o:{
+      patches = (o.patches or []) ++ [
+        ./fix1.patch
+      #  ./fix2.patch
+      ];
+    });
+  };
+  use_curl_git = buildTests {
+    name = "curl-git-tests";
+    curl = curl.overrideAttrs (o:{
+      src = builtins.fetchGit https://github.com/curl/curl;
+      nativeBuildInputs = (o.nativeBuildInputs or []) ++ [ autoreconfHook ];
+      preConfigure = ":"; # disable fixups we do that are only needed on release tarballs
+    });
+  };
 }
